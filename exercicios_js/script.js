9f535e9523f5ab6f741b866ec1569a8e90b1778c
David Silva <104469708+davidsilvafreire@users.noreply.github.com>
2022-06-21 14:04:30 -0300

    atualizacao01

--- exercicios_js/script.js
+++ exercicios_js/script.js
@@ -2,7 +2,7 @@ function verificar(){
    var data = new Date()          //Data atual do sistema
    var ano = data.getFullYear()  //Ano atual do sistema
    var formularioAno = document.getElementById('textoAno')  //ano de nascimento
-   var Res = document.querySelector('div#resultado')
+   var res = document.querySelector('div#resultado')
 
    //Se o comprimento do valor digitado na caixa de texto(textoAno) for igual a 0
    //ou se o ano atual do sistema for menor que o valor digitado na caixa de texto...
@@ -14,8 +14,8 @@ function verificar(){
     var idade = ano - Number(formularioAno.value) 
     //o valor digitado na caixa de texto(formularioAno) menos o ano atual do sistema é a idade!
     var genero = ''  //comecou com uma string vazia 
-    var img = document.createAttribute('img')
-    img.('id', 'foto')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
 
 
     if (Sexo[0].checked){
@@ -24,6 +24,7 @@ function verificar(){
         img.setAttribute('src','foto-homem-jovem.jpg')
         //Jovem
      } else if (idade < 50) {
+      img.setAttribute('src', 'foto-homem-adulto.jpg')
         //adulto
      } else {
         //idoso
@@ -31,20 +32,20 @@ function verificar(){
      //Se o que estiver marcado(checked) masculino[0], então é HOMEM
     } else if(Sexo[1].checked){
      genero = 'mulher'
-     if (idade >=0 && idade < 10 ) {
-        //Criança
-     } else if (idade < 21) {
+     if (idade >=20 && idade < 30 ) {
+      img.setAttribute('src','foto-mulher-jovem.jpg')
         //Jovem
-     } else if (idade < 51) {
-        //adulto
-     } else {
+     } else if (idade < 50) {
+      img.setAttribute('src','foto-mulher-adulta.jpg')
+        //Adulto
+     }  else {
         //idoso
      }
-     //Se não que estiver marcado(checked) feminino[1], então é MULHER
+     //Se o que estiver marcado(checked) feminino[1], então é MULHER
     }
-    resultado.style.textAlign = 'center'
-    resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-    resultado.appendChild(img)
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
    }
   
 }

9927f336166e13b3de87694f1a1309e9f8475dfb
David Silva <104469708+davidsilvafreire@users.noreply.github.com>
2022-06-23 17:58:05 -0300

    atualizacao exe04_js

--- exe04_js/script.js
+++ exe04_js/script.js
@@ -1,25 +1,19 @@
-let numero = document.getElementById('formulario-numero')
-//
-let listas = document.querySelector('select#form-lista')
-// 
+let num = document.querySelector('input#formulario-numero')
+let lista = document.querySelector('select#form-lista')
 let resultado = document.querySelector('div#res')
-//
-let valoresAnalisados = []
-//são os valores que serão analisados no vetor dentro das chaves[]
+let valores = []   //Array (vetores)
 
-
-//Esta funcao serve para saber se é um número ou não
-function isNumero(n){
-      if (Number(n) >= 1 && Number(n) <= 100){
+function éNumero(n){ //funcao do numero
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
-      } else {
+    } else {
         return false
-      }
-}
+    }
+           
+} 
 
 
-function inLista(n, l){
-//se a posição do numero encontrado na lista for diferente('!='), não encontrará nenhum elemento(-n) na lista
+function emLista(n, l){  //funcao da lista
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -27,12 +21,56 @@ function inLista(n, l){
     }
 }
 
-/*Esta função irá analisar os números para ver se estão na lista*/
-function adicionar() {
-    if (isNumero(numero.value) && inLista(numero.value, valoresAnalisados)) {
-        window.alert('Tudo OK!')
-        
+
+function adicionar(){  //funcao do evento [adicionar]
+//se o numero nao estiver na lista irá adicionar a ela. ok!    
+    if (éNumero(num.value) && !emLista(num.value, valores)){
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado.`
+       lista.appendChild(item)
+       resultado.innerHTML = ''  //quando adicionar o valor, tem que limpar o resultado
     } else {
+//se não, irá disparar a mensagem abaixo!        
         window.alert('Valor inválido ou já encontrado na lista')
+    }
+    //serve para limpar a caixa de texto após adicionar o valor
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        //total de elementos
+        let total = valores.length
+
+        //até o momento o maior e menor valor, é primeiro na lista!
+        let maior = valores[0]
+        let menor = valores[0]
+        
+        let soma = 0
+        let media = 0
+
+
+//criar um for para analisar um por um qual é o maior ou menor
+        for(let pos in valores){
+            soma += valores[pos]
+            
+
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor) menor = valores[pos]
+        }
+
+        //mostrar na tela o resultado, vazio, por enquanto...
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>Total de valores cadastrados: [ <strong>${total}</strong> ]</p>`
+        //informar o menor ou maior valor na tela!
+        resultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        resultado.innerHTML += `<p>O menor vsalor informado foi ${menor}.</p>`
+        resultado.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        resultado.innerHTML += `<p>subtraindo todos os valores, temos ${subtrair}</p>`
     }
 }
